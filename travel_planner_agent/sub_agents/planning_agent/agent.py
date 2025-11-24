6727028f42736c3833879f899c8ad34d1e9d471d
jayhan <jihun8865@gmail.com>
2025-11-24 18:37:38 +0800

    fix: add ThinkingConfig and update first user prompt

--- travel_planner_agent/sub_agents/planning_agent/agent.py
+++ travel_planner_agent/sub_agents/planning_agent/agent.py
@@ -1,7 +1,9 @@
 """Configures the planning agent instance."""
 
 from google.adk.agents.llm_agent import Agent
+from google.adk.planners import BuiltInPlanner
 from google.adk.tools import google_search
+from google.genai.types import ThinkingConfig
 
 # from google.adk.models.lite_llm import LiteLlm
 
@@ -10,11 +12,16 @@ from travel_planner_agent.tools.search import search_flight, search_hotel
 
 # search_web
 
+thinking_config = ThinkingConfig(include_thoughts=True, thinking_budget=256)
+
+planner = BuiltInPlanner(thinking_config=thinking_config)
+
 planning_agent = Agent(
     # model=LiteLlm(model=f"{getenv('OLLAMA_MODEL_ID', 'ollama_chat/qwen3:8b')}"),
     model="gemini-2.5-flash",
     name="planning_agent",
     description="Specialist that collects requirements and drafts trip plans for travelers.",
     instruction=prompt.PLANNING_AGENT_INSTR,
+    planner=planner,
     tools=[search_flight, search_hotel, google_search],
 )
